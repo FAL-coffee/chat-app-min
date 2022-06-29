996e5ce9481e7f364a73185be586acc7f30801cb
FAL-coffee <fal.engineer.2001@gmail.com>
2022-06-30 04:02:46 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
 import type { NextPage } from "next";
 import { db, onSnapshot, query } from "@/plugin/firebase";
 import React, { useEffect, useState } from "react";
-import { addDoc, collection, orderBy } from "firebase/firestore";
+import { addDoc, collection, orderBy, Timestamp } from "firebase/firestore";
+
+type TMessage = {
+  value: string;
+  at: Timestamp;
+};
 
 const Room: NextPage = () => {
-  const [messages, setMessages] = useState<string[]>([]);
-
-  const post = async (value: string) => {
+  const [messages, setMessages] = useState<TMessage[]>([]);
+  const [value, setValue] = useState<string>("");
+  const post = async () => {
     const data = {
       value: value,
-      postedAt: new Date(),
+      at: new Date(),
     };
+    console.log(data);
     const messagesRef = await collection(db, "messages");
     await addDoc(messagesRef, {
       ...data,
@@ -18,23 +24,30 @@ const Room: NextPage = () => {
   };
 
   useEffect(() => {
-    const q = query(collection(db, "messages"), orderBy("postedAt", "desc"));
+    const tempMessages: TMessage[] = [];
+    const q = query(collection(db, "messages"), orderBy("at", "desc"));
     onSnapshot(q, (snapshot) => {
       snapshot
         .docChanges()
         .reverse()
         .forEach(async (change) => {
           if (change.type === "added") {
+            await tempMessages.push({ ...(change.doc.data() as TMessage) });
           }
+          await setMessages([...tempMessages]);
         });
     });
   }, []);
 
   return (
     <>
-      {messages.map((message, i) => {
-        <div key={i}>{message}</div>;
-      })}
+      {messages.map((message, i) => (
+        <div key={i}>{message.value}</div>
+      ))}
+      <div>
+        <input value={value} onChange={(e) => setValue(e.target.value)} />
+        <button onClick={post}>送信</button>
+      </div>
     </>
   );
 };
